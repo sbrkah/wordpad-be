b1edf2c290ceb14419ee42c78778ae5ffe6b0c30
sbrkah <muh.subarkah.911@gmail.com>
2025-11-08 14:33:57 +0700

    Update: Class based generation

--- main.js
+++ main.js
@@ -1,61 +1,87 @@
-const fs = require('fs');
+import { writeFileSync } from 'fs';
+import { fileReader, loadHistory } from './utils.js';
+import { console } from 'inspector';
 
-async function loadHistory() {
-    try {
-        const data = await fs.promises.readFile('./api/history.json', 'utf8');
-        const history = JSON.parse(data);
-        return history;
-    } catch (error) {
-        console.error('Error loading history:', error);
-        return [];
+let crntry = 0;
+let maxtry = 2800;
+
+class Generate {
+    static l = 'xqvz'.split('');        // Least common letter
+    static t = 'ageinrou'.split('');    // Most common letter in KBBI
+    static alphabet = 'etaoinshrdlcumwfgypbvkjxqz'.split('');
+    static r = this.alphabet.filter(char => !this.t.includes(char) && !this.l.includes(char));
+
+    static ml = '';
+    static bl = '';
+    static wordLib = [];
+    static history = [];
+    static possibleSet = new Set(this.wordLib.join(''));
+
+    static updatePossibleSet(){
+        let key = new Set(this.ml + this.bl);
+        function strHasAll(str){
+            let set = new Set(str);
+            set.forEach(char => {if(!key.has(char))return false});
+            return true;
+        }
+
+        this.wordLib = this.wordLib.filter(item => strHasAll(item));
+        this.possibleSet = new Set(this.wordLib.join(''));
+    }
+
+    static #getDict(){
+        return {
+            builderLetter: this.bl,
+            mainLetter: this.ml,
+            date: new Date().toISOString().split('T')[0]
+        }
+    }
+
+    static async Run() {
+        this.history = await loadHistory("./api/history.json");
+        this.wordLib = await fileReader("./corelation/xxx.txt", "utf8", "\n");
+        const mlhistory = [...this.history.map(item => item.main)];
+        
+        while(true) {
+            this.ml = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+            if(!mlhistory.includes(this.ml)) {
+                this.updatePossibleSet(); break;
+            }
+        }
+
+        while(this.bl.length < 6 && crntry < maxtry) {
+            while (crntry < maxtry){
+                const char = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+                if (this.ml !== char && !this.bl.includes(char) && this.possibleSet.has(char)) {
+                    this.bl += char; break;
+                }
+
+                crntry++;
+            }
+
+            this.updatePossibleSet();
+        }
+
+        return this.#getDict();
+    }
+
+    static saveDailySet(){
+        const dailySet = this.#getDict();
+        let updatedHistory = [dailySet, ...this.history];
+        writeFileSync('./api/daily-set.json', JSON.stringify(dailySet, null, 2));
+
+        if(updatedHistory.length > 7) {
+            updatedHistory = updatedHistory.slice(0, 7);
+        }
+        
+        writeFileSync('./api/history.json', JSON.stringify(updatedHistory, null, 2));
+        console.log('Daily set updated successfully!', dailySet);
     }
 }
 
-async function generateDailySet(history) {
-    const mainLetterHistory = [...history.map(item => item.main)];
-    const commonLetters = 'etaoinshrdlcumwfgypbvkjxqz';
-    const corelation = JSON.parse(await fs.promises.readFile('./corelation.json', 'utf8'));
-    const arraySet = [];
-    let mainLetter = '';
-
-    while (1){
-        mainLetter = commonLetters[Math.floor(Math.random() * commonLetters.length)];
-        if (!mainLetterHistory.includes(mainLetter)) {
-            break;
-        }
-    }
-    // Generate 8 unique letters
-    while (arraySet.length < 7) {
-        const char = commonLetters[Math.floor(Math.random() * commonLetters.length)];
-        // make sure the last chat is not in the main letter history
-        if (!arraySet.includes(char) && char !== mainLetter && corelation[`${mainLetter}${char}`] > 0) {
-            arraySet.push(char);
-        }
-    }
-    
-    const builderLetter = arraySet.join('');
-    
-    return { 
-        builderLetter, 
-        mainLetter, 
-        date: new Date().toISOString().split('T')[0] 
-    };
-}
-
-// Main execution
 async function main() {
-    const history = await loadHistory();
-    const dailySet = await generateDailySet(history);
-    
-    fs.writeFileSync('./api/daily-set.json', JSON.stringify(dailySet, null, 2));
-    
-    const updatedHistory = [dailySet, ...history];
-    if(updatedHistory.length > 7) {
-        updatedHistory.pop();
-    }
-    
-    fs.writeFileSync('./api/history.json', JSON.stringify(updatedHistory, null, 2));
-    console.log('Files updated successfully!');
+    const dailySet = await Generate.Run();
+    Generate.saveDailySet();
 }
 
 main().catch(console.error);
---
+++ utils.js
@@ -0,0 +1,27 @@
+import { readFile } from 'fs/promises';
+
+async function fileReader(filepath, encoding = "utf8", split = NaN) {
+    try {
+        const data = await readFile(filepath, encoding);
+        if (split) {
+            return data.split(split);
+        }
+        return await readFile(filepath, encoding);
+    } catch (error) {
+        console.error("Error reading file:", error);
+        throw error;
+    }
+}
+
+async function loadHistory(filepath) {
+        try {
+            const data = await fileReader(filepath, "utf8");
+            const history = JSON.parse(data);
+            return history;
+        } catch (error) {
+            console.error('Error loading history:', error);
+            return [];
+        }
+    }
+
+export { fileReader, loadHistory };
